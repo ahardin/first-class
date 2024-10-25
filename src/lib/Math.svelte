28e116bb6e88dcb7392a5cf06c52cddc5020ffeb
Alex Hardin <alexchardin@gmail.com>
2024-10-24 22:43:39 -0700

    stubbed out much more of the game

--- src/lib/Math.svelte
+++ src/lib/Math.svelte
@@ -1,39 +1,96 @@
 <script lang="ts">
+	// set initial values for the variables representing the math problem
 	let number1: number = 2;
 	let number2: number = 3;
+	let answer: number | string = '';
+	let operators = ['+', '-', '*', '/'];
+	let operator: string = '';
 
-	let answer = 0;
+	// set initial values for the variables representing the game state
+	let isCorrect: boolean | null = null;
+	let score: number = 0;
+	let timeLeft: number = 30;
+	let gameActive: boolean = false;
 
-	function check() {
-		const realAnswer = number1 + number2;
+	function startGame() {
+		score = 0;
+		timeLeft = 30;
+		gameActive = true;
+		generateNewQuestion();
 
-		if (realAnswer == answer) {
+		// start up a timer using setInterval
+		// the funny looking arrow is a function
+		// it will run every 1000 milliseconds (1 second)
+		const timer = setInterval(() => {
+			timeLeft--;
+
+			if (timeLeft <= 0) {
+				clearInterval(timer);
+				gameActive = false;
+			}
+		}, 1000); // 1000 milliseconds = 1 second
+	}
+
+	function checkAnswer() {
+		// calculate the real answer based on the operator
+		let realAnswer: number = 0;
+
+		if (operator === '+') {
+			realAnswer = number1 + number2;
+		}
+
+		if (realAnswer === Number(answer)) {
 			isCorrect = true;
+			score++;
+			generateNewQuestion();
 		} else {
 			isCorrect = false;
 		}
+		
+		answer = '';
+	}
+
+	function generateNewQuestion() {
+		number1 = Math.floor(Math.random() * 10) + 1;
+		number2 = Math.floor(Math.random() * 10) + 1;
+		operator = '+';
+	}
+
+	// hmmm, maybe we should use this function?
+	function pickOperator() {
+		return operators[Math.floor(Math.random() * operators.length)];
 	}
 </script>
 
-<h2>Math Challenge!</h2>
-<p>You have 30 seconds to answer as many questions as possible.</p>
+<div class="space-y-4 w-full md:max-w-96 mx-auto">
+	<h2>Math Challenge!</h2>
+	<p>You have 30 seconds to answer as many questions as possible.</p>
 
-<p>Winner gets unlimited respect.</p>
+	<p>Winner gets unlimited respect.</p>
 
-<div class="text-center">
-	{number1} + {number2} = __
+	{#if !gameActive}
+		<button class="btn btn-primary" on:click={startGame}>Start Game</button>
+	{:else}
+		<div class="text-center">
+			<p>Score: {score} | Time Left: {timeLeft}s</p>
+			{number1} + {number2} = __
+		</div>
+
+		<div>
+			<input
+				class="input input-bordered"
+				type="number"
+				bind:value={answer}
+				on:keypress={(e) => e.key === 'Enter' && checkAnswer()}
+			/>
+		</div>
+
+		<div>
+			<button class="btn btn-secondary" on:click={checkAnswer}>Check</button>
+		</div>
+
+		<p class="text-success">Nice job, you got it right!</p>
+
+		<p class="text-error">Oops! Try again!</p>
+	{/if}
 </div>
-
-<div>
-	<input class="input input-bordered" type="text" bind:value={answer} />
-</div>
-
-<div>
-	<button class="btn btn-secondary" on:click={check}>Check</button>
-</div>
-
-{#if isCorrect}
-	<p>Nice job, you got it right!</p>
-{:else}
-	<p>Totally wrong!</p>
-{/if}
