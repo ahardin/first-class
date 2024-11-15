92f2e6c480d98fb1d080612312ff62819b982d23
Alex Hardin <alexchardin@gmail.com>
2024-11-15 11:33:20 -0800

    presentation

--- src/lib/Math.svelte
+++ src/lib/Math.svelte
@@ -1,20 +1,38 @@
 <script lang="ts">
+	import { Fireworks, type FireworksOptions } from '@fireworks-js/svelte';
+	import { saveScore } from './data';
+
+	let fw: Fireworks;
+	let showFireworks = false;
+	let options: FireworksOptions = {
+		opacity: 0.5,
+		intensity: 3
+	};
+
+	function startFireworks() {
+		showFireworks = true;
+
+		setTimeout(() => {
+			showFireworks = false;
+		}, 6500);
+	}
+
 	// set initial values for the variables representing the math problem
 	let number1: number = 2;
 	let number2: number = 3;
 	let answer: number | string = '';
-	let operators = ['+', '-', '*', '/'];
+	let operators = ['+', '-', '*'];
 	let operator: string = '';
 
 	// set initial values for the variables representing the game state
-	let isCorrect: boolean | null = null;
+	let isCorrect: boolean | undefined;
 	let score: number = 0;
 	let timeLeft: number = 30;
 	let gameActive: boolean = false;
 
 	function startGame() {
 		score = 0;
-		timeLeft = 30;
+		timeLeft = 45;
 		gameActive = true;
 		generateNewQuestion();
 
@@ -27,6 +45,7 @@
 			if (timeLeft <= 0) {
 				clearInterval(timer);
 				gameActive = false;
+				isGameOver = true;
 			}
 		}, 1000); // 1000 milliseconds = 1 second
 	}
@@ -39,41 +58,67 @@
 			realAnswer = number1 + number2;
 		}
 
+		if (operator === '-') {
+			realAnswer = number1 - number2;
+		}
+
+		if (operator === '*') {
+			realAnswer = number1 * number2;
+		}
+
+		if (operator === '/') {
+			realAnswer = number1 / number2;
+		}
+
 		if (realAnswer === Number(answer)) {
 			isCorrect = true;
-			score++;
+			score = score + 100;
+			startFireworks();
 			generateNewQuestion();
 		} else {
 			isCorrect = false;
 		}
-		
+
 		answer = '';
 	}
 
 	function generateNewQuestion() {
 		number1 = Math.floor(Math.random() * 10) + 1;
 		number2 = Math.floor(Math.random() * 10) + 1;
-		operator = '+';
+		operator = pickOperator();
 	}
 
 	// hmmm, maybe we should use this function?
 	function pickOperator() {
 		return operators[Math.floor(Math.random() * operators.length)];
 	}
+
+	let name: string = '';
+	let isGameOver: boolean = false;
+
+	async function save() {
+		await saveScore('blueberry', name, score);
+		isGameOver = false;
+	}
 </script>
 
+{#if showFireworks}
+	<Fireworks bind:this={fw} {options} class="fireworks" />
+{/if}
 <div class="space-y-4 w-full md:max-w-96 mx-auto">
-	<h2>Math Challenge!</h2>
-	<p>You have 30 seconds to answer as many questions as possible.</p>
+	<h2>ARE U StUpId?</h2>
+	<p>find out now!</p>
 
-	<p>Winner gets unlimited respect.</p>
+	<p>by playing our game!</p>
 
 	{#if !gameActive}
-		<button class="btn btn-primary" on:click={startGame}>Start Game</button>
+		<button class="btn btn-primary" on:click={startGame}>PuSH nOW</button>
 	{:else}
 		<div class="text-center">
 			<p>Score: {score} | Time Left: {timeLeft}s</p>
-			{number1} + {number2} = __
+			{number1}
+			{operator}
+			{number2} = __
 		</div>
 
 		<div>
@@ -86,11 +131,36 @@
 		</div>
 
 		<div>
-			<button class="btn btn-secondary" on:click={checkAnswer}>Check</button>
+			<button class="btn btn-secondary" on:click={checkAnswer}>spadoinkadoink check</button>
 		</div>
 
-		<p class="text-success">Nice job, you got it right!</p>
-
-		<p class="text-error">Oops! Try again!</p>
+		{#if isCorrect != undefined}
+			{#if isCorrect}
+				<p class="text-success">Dave</p>
+			{:else}
+				<p class="text-error">YOU ARE A IDIOT SANDWICH</p>
+			{/if}
+		{/if}
 	{/if}
+
+	{#if isGameOver}
+		<div>
+			<h3>Save your score!</h3>
+			<input bind:value={name} class="input input-bordered" type="text" placeholder="Your name" />
+			<button type="button" class="btn btn-primary" on:click={save}>Save</button>
+		</div>
+	{/if}
+
+	<div><a href="/scores">View Scoreboard</a></div>
 </div>
+
+<style>
+	:global(.fireworks) {
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		z-index: -1;
+	}
+</style>
---
+++ src/lib/data.ts
@@ -0,0 +1,21 @@
+import { createClient } from '@supabase/supabase-js';
+
+export async function saveScore(team: string, name: string, score: number) {
+    try {
+        const supabase = createClient('https://phfllbfvtccryshrtxgo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoZmxsYmZ2dGNjcnlzaHJ0eGdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2MjQ1ODgsImV4cCI6MjA0NzIwMDU4OH0.ccaA_4cInFE_FJestHHjtdc2tHNmO03GDOXt_pOyotA');
+
+        await supabase.from('scores').insert({ name: name, team: 'blueberry', score: score }).select();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function loadScores(team: string) {
+    try {
+        const supabase = createClient('https://phfllbfvtccryshrtxgo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoZmxsYmZ2dGNjcnlzaHJ0eGdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2MjQ1ODgsImV4cCI6MjA0NzIwMDU4OH0.ccaA_4cInFE_FJestHHjtdc2tHNmO03GDOXt_pOyotA');
+
+        return await supabase.from('scores').select('*').eq('team', team).order('name', { ascending: true });
+    } catch (e) {
+        console.log(e);
+    }
+} 
